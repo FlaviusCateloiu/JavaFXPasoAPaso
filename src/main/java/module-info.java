13429f076dbb99cc3b3dfc9b8b3114a7b3d98625
flaars <fcateloiu08@gmail.com>
2022-11-29 18:51:40 +0100

    Hola

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.javafxpasoapaso {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.javafxpasoapaso to javafx.fxml;
     exports com.example.javafxpasoapaso;
 }
