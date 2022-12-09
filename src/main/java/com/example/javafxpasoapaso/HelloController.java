a576c3db861cc58bbbb804f6c55d5946e5e7ca56
flaars <fcateloiu08@gmail.com>
2022-12-09 17:44:54 +0100

    Tengo la interfaz falta los funcionamientos y mas cosas.

--- src/main/java/com/example/javafxpasoapaso/HelloController.java
+++ src/main/java/com/example/javafxpasoapaso/HelloController.java
@@ -1,16 +1,27 @@
 package com.example.javafxpasoapaso;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.nio.channels.Pipe;
 
 public class HelloController {
     @FXML
     private TextField tFCode;
+    @FXML
     private TextField tFFirstName;
+    @FXML
     private TextField tFLastName;
+    @FXML
     private DatePicker dPBirthdate;
+    @FXML
+    private GridPane gPSaveAndCancel;
 
 
     @FXML
@@ -19,5 +30,40 @@ public class HelloController {
         tFFirstName.setDisable(false);
         tFLastName.setDisable(false);
         dPBirthdate.setDisable(false);
+        gPSaveAndCancel.setVisible(true);
+    }
+
+    @FXML
+    protected void onCheckOption() {
+        tFCode.setDisable(true);
+        tFFirstName.setDisable(true);
+        tFLastName.setDisable(true);
+        dPBirthdate.setDisable(true);
+        gPSaveAndCancel.setVisible(false);
+    }
+
+    @FXML
+    protected void onCancelOption() {
+        tFCode.setDisable(true);
+        tFCode.setText("");
+        tFFirstName.setDisable(true);
+        tFFirstName.setText("");
+        tFLastName.setDisable(true);
+        tFLastName.setText("");
+        dPBirthdate.setDisable(true);
+        dPBirthdate.setValue(null);
+        gPSaveAndCancel.setVisible(false);
+    }
+
+    @FXML
+    protected void onSearchPerson() {
+        tFCode.setDisable(false);
+        gPSaveAndCancel.setVisible(false);
+        gPSaveAndCancel.setVisible(true);
+    }
+
+    @FXML
+    protected void onToExitProgram() {
+        System.exit(0);
     }
 }
